eaf9daf5f482446d882ad9ede6a99c04b29d12dd
Christoph Frick <cf@ofnir.net>
2018-02-11 15:03:54 +0100

    Add wrepl.rebel-readline

--- project.clj
+++ project.clj
@@ -2,7 +2,8 @@
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [net.ofnir/wrepl "0.1.0-SNAPSHOT"]
                  [net.ofnir/wrepl.puget "0.1.0-SNAPSHOT"]
-                 [net.ofnir/wrepl.pomegranate "0.1.0-SNAPSHOT"]]
+                 [net.ofnir/wrepl.pomegranate "0.1.0-SNAPSHOT"] 
+                 [net.ofnir/wrepl.rebel-readline "0.1.0-SNAPSHOT"]]
   :main ^:skip-aot wrepl.main
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
